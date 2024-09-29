615fef484c4492e990f2e45df13855922247603f
TheTechWizard1 <professorgoodno1@gmail.com>
2024-09-29 23:35:39 +0600

    fixed some files

--- Poster/asgi.py
+++ Poster/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Afisha project.
+ASGI config for Poster project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Afisha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Poster.settings')
 
 application = get_asgi_application()
